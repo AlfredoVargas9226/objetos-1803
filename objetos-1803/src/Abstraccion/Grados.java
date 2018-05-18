4ff6cbd0d15b15cb2ff001786a458d7705eaee46
T-101 <T-101@PC223.mshome.net>
2018-05-17 19:45:43 -0500

    solución coversion parte 1

---
+++ objetos-1803/src/Abstraccion/Grados.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Abstraccion;
+
+/**
+ *
+ * @author T-101
+ */
+public class Grados {
+    private float centigrados;
+    private float farenheint;
+
+    public float getCentigrados() {
+        return centigrados;
+    }
+
+    public void setCentigrados(float centigrados) {
+        this.centigrados = centigrados;
+    }
+
+    public float getFarenheint() {
+        return farenheint;
+    }
+
+    public void setFarenheint(float farenheint) {
+        this.farenheint = farenheint;
+    }
+
+}
+
