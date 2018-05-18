d0034215da226a828f367b256fba12bf84e1e4a0
AlfredoVargas9226 <fredi_9226@hotmail.com>
2018-05-17 19:03:12 -0500

    Update Objetos1803.java

--- objetos-1803/src/objetos/pkg1803/Objetos1803.java
+++ objetos-1803/src/objetos/pkg1803/Objetos1803.java
@@ -16,6 +16,7 @@ public class Objetos1803 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        system.out.printl("una actualizacion desde el repositorio");
     }
     
 }
