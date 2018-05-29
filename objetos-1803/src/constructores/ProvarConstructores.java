3cb969290290562ba698c01c0893856c4806d668
T-101 <T-101@PC222>
2018-05-28 19:35:45 -0500

    tipos de datos

---
+++ objetos-1803/src/constructores/ProvarConstructores.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package constructores;
+
+/**
+ *
+ * @author T-101
+ */
+public class ProvarConstructores {
+    public static void main(String[] args) {
+        
+        Fruta f1=new Fruta();
+        System.out.println("el nombre de la fruta es"+f1.nombre);
+        System.out.println("el precio de la fruta es"+f1.precio);
+       float valor;
+        //System.out.println(valor);
+    }
+}
