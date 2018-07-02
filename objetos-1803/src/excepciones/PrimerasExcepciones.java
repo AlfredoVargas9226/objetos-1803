c68e4ebe162782a5969e1d5bcd32a4aed5682791
T-101 <T-101@PC237>
2018-07-02 18:55:58 -0500

    excepciones

---
+++ objetos-1803/src/excepciones/PrimerasExcepciones.java
@@ -0,0 +1,19 @@
+
+package excepciones;
+
+
+public class PrimerasExcepciones {  //metodo main
+    
+    public static void main(String[] args) {
+        int a=2;
+        int b=0;
+      // System.out.println(a/b);
+       int arreglo[]={3,4,2};
+       System.out.println("hola mundo");
+      // System.out.println(arreglo[3]);
+      String numeroMalo="4";
+      int numeroBueno=Integer.parseInt(numeroMalo);
+        System.out.println(numeroBueno*2);
+       System.out.println("no me veras");
+    }
+}
