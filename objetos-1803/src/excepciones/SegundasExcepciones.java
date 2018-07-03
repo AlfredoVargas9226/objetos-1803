bd32bcfbb6336cdcdf9d40c28094ac292b21c085
T-101 <T-101@PC237>
2018-07-02 19:44:23 -0500

    excepciones

--- objetos-1803/src/excepciones/SegundasExcepciones.java
+++ objetos-1803/src/excepciones/SegundasExcepciones.java
@@ -14,12 +14,9 @@ public class SegundasExcepciones {
         System.out.println(a/b);
         System.out.println(numeroBueno*2);
         System.out.println(arreglo [0]);
-    }catch(NumberFormatException e){ //cuqando se cierra inmediatamente el try se pone mi }cath y lo cierro{
-            System.out.println("¿podrias meter un numero valido?");
-}catch(ArithmeticException e){
-            System.out.println("no se aceptan divisiones entre 0");
+    
 }catch(Exception e){
-            System.out.println("esto ocurrio"+e.getMessage()); //este es el que muestra el mensaje de la excepcion
+System.out.println("veamos que excepcion ocurre"+e.getMessage()); //este es el que muestra el mensaje de la excepcion
 }
 }
 }
